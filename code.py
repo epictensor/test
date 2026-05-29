07cf2305c8d6df5448409dc0ea5348fa9403339a
Epic Tensor <epictensor@cookingtao.com>
2026-05-29 19:17:47 +0545

    update code.py

--- code.py
+++ code.py
@@ -1,7 +1,10 @@
 import time
+import os
 
-num = 0
+num=0
+openai_key = os.environ.get("OPENAI_API_KEY")
+
 while True:
-    print(num)
+    print(f"{num}: {openai_key}")
     num += 10
     time.sleep(10)
