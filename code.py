f244f1bf69e962aca020fd6dc5e0a962659ac061
Epic Tensor <epictensor@cookingtao.com>
2026-05-29 22:58:07 +0545

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,13 @@
-import time
-import os
+import os, sys, time
 
-num=0
+num = 0
+print(f"PID: {os.getpid()}, PPID: {os.getppid()}")
+print(f"Executable: {sys.executable}")
+print(f"OPENAI_API_KEY in os.environ: {'OPENAI_API_KEY' in os.environ}")
+print(f"Environ keys: {sorted(os.environ.keys())}")
 openai_key = os.environ.get("OPENAI_API_KEY")
 
 while True:
-    print(f"{num}: {openai_key}")
-    num += 10
-    time.sleep(10)
+  print(f"{num}: {openai_key}")
+  num += 10
+  time.sleep(10)
